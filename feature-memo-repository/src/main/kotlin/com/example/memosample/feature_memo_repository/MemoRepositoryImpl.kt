0184eba8cd3ec092b520a9eaa7a23906370bc035
hasegawa <gouta.gorogoro@gmail.com>
2022-07-04 13:48:58 +0900

    feature #12 Repositoryのコンストラクタに@Injectを追加

--- feature-memo-repository/src/main/kotlin/com/example/memosample/feature_memo_repository/MemoRepositoryImpl.kt
+++ feature-memo-repository/src/main/kotlin/com/example/memosample/feature_memo_repository/MemoRepositoryImpl.kt
@@ -1,8 +1,9 @@
 package com.example.memosample.feature_memo_repository
 
 import com.example.memosample.feature_memo_repository.model.MemoModel
+import javax.inject.Inject
 
-internal class MemoRepositoryImpl: MemoRepository {
+internal class MemoRepositoryImpl @Inject constructor(): MemoRepository {
   override suspend fun fetchMemoList(): List<MemoModel> {
     TODO()
   }
